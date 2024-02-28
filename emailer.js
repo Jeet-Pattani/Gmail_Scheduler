da19cc7fe6031c3bcc0363c734553c70a039f659
Keval Indrodia <93391700+Jeet-Pattani@users.noreply.github.com>
2024-02-28 12:25:21 +0530

    timezone update

--- emailer.js
+++ emailer.js
@@ -6,6 +6,7 @@ const { sendEmail } = require('../src/services/gmailApiServices');
 const fs = require('fs').promises;
 const path = require('path');
 const schedule = require('node-schedule');
+const moment = require('moment-timezone');
 
 let scheduledEmails = {}; // Object to store scheduled emails by their IDs
 
@@ -15,13 +16,15 @@ async function loadFileContent() {
     try {
         const fileContent = await fs.readFile(filePath, 'utf8');
         const data = JSON.parse(fileContent);
-        const unsentEmails = data.unsent.filter(email => !email.sentEmail && new Date(email.date) > new Date());
+        const unsentEmails = data.unsent.filter(email => !email.sentEmail);
         unsentEmails.forEach((email) => {
             const { id, recipient, subject, message, custName,custPhNo, carModel, date } = email;
+            const systemTimeZone = moment.tz.guess();
+            const singaporeTime = moment.tz(date, 'Asia/Kolkata').tz(systemTimeZone).format(); // Convert to Singapore time
             finalMessage = `Message Description: ${message}.\n\n Customer Name: ${custName}\n\n Mobile No.: ${custPhNo}\n\n Car Model: ${carModel}`;
             const msg = `TO: ${recipient}\nSubject: ${subject}\nContent-Type: text/html; charset=utf-8\n\n${finalMessage}`;
             if (!scheduledEmails[id]) { // Check if the email ID is not already scheduled
-                schedule.scheduleJob(date, async () => {
+                schedule.scheduleJob(singaporeTime, async () => {
                     try {
                         await sendEmail(auth, msg);
                         console.log(`Email sent to ${recipient} - Subject: ${subject}`);
@@ -34,7 +37,7 @@ async function loadFileContent() {
                         console.error('Error sending email:', error);
                     }
                 });
-                console.log(`Email scheduled for ${date} - Subject: ${subject}`);
+                console.log(`Email scheduled for ${date} (IST) / ${singaporeTime} (SGT) - Subject: ${subject}`);
                 scheduledEmails[id] = true; // Mark the email ID as scheduled
             }
         });
@@ -47,6 +50,6 @@ async function loadFileContent() {
 (async () => {
     while (true) {
         await loadFileContent();
-        await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 minute before checking again
+        await new Promise(resolve => setTimeout(resolve, 10000)); // Wait for 1 minute before checking again
     }
 })();
